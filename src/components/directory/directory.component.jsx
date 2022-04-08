68ab0e92afc85cee418a8cadfa0eb2f4935491f2
Harish Krishnan <h.krishnan@sap.com>
2022-04-08 17:05:30 +0530

    Fix style conflict

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,12 +1,14 @@
-import CategoryItem from '../category-item/category-item.component';
-import './directory.styles.scss'
+import DirectoryItem from '../directory-item/directory-item.component';
+import './directory.styles.scss';
 
 const Directory = ({ categories }) => {
-  return <div className="directory-container">
-    {categories.map((category) => (
-      <CategoryItem key={category.id} category={category} />
-    ))}
-  </div>
-}
+  return (
+    <div className='directory-container'>
+      {categories.map((category) => (
+        <DirectoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
 
-export default Directory
+export default Directory;
